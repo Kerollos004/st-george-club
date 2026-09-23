966f0f34f3482518a3f02c43cb0356800c614b8d
kero melad <meladkerollos0@gmail.com>
2026-09-23 12:10:31 +0300

    change rounte||

--- app/components/dashboradComponents/DasboardMember.tsx
+++ app/components/dashboradComponents/DasboardMember.tsx
@@ -57,7 +57,7 @@ export default function DasboardMember({ member }: { member: MemberType }) {
                 }} className={`p-3 text-white rounded-2xl ${activeState ? "bg-green-700" : "bg-red-700"}`}>
                     {activeState ? "إلغاء التفعيل" : "تفعيل"}
                 </button>
-                <Link  className='p-3 rounded-2xl bg-blue-600 hover:bg-blue-800'  href={`/}`}> معاينه </Link>
+                <Link  className='p-3 rounded-2xl bg-blue-600 hover:bg-blue-800'  href={`/`}> معاينه </Link>
             </div>
         </div>
     )
